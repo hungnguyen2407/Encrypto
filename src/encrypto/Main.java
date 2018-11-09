983313d75e4f156c312e77e338053dedcc71da69
Hung <14130047@st.hcmuaf.edu.vn>
2018-11-09 23:19:07 +0700

    v1.4.9 - improve code

--- src/encrypto/Main.java
+++ src/encrypto/Main.java
@@ -5,15 +5,14 @@ import javafx.embed.swing.SwingFXUtils;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-
-
 import javafx.stage.Stage;
+
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 public class Main extends Application {
 
-    static final String APP_VERSION = "1.4.8";
+    static final String APP_VERSION = "1.4.9";
 
     public static void main(String[] args) {
         launch(args);
@@ -32,18 +31,19 @@ public class Main extends Application {
         primaryStage.getIcons().add(new javafx.scene.image.Image(Main.class.getResourceAsStream("icon.png")));
 
         //set icon for macOS
-        try {
-            Class util = Class.forName("com.apple.eawt.Application");
-            Method getApplication = util.getMethod("getApplication");
-            Object application = getApplication.invoke(util);
-            Class params[] = new Class[1];
-            params[0] = java.awt.Image.class;
-            Method setDockIconImage = util.getMethod("setDockIconImage", params);
-            setDockIconImage.invoke(application, SwingFXUtils.fromFXImage(new javafx.scene.image.Image(Main.class.getResourceAsStream("icon.png")), null));
-        } catch (ClassNotFoundException | NoSuchMethodException | InvocationTargetException | IllegalAccessException e) {
-            e.printStackTrace();
+        if ("Mac OS X".equals(System.getProperty("os.name"))) {
+            try {
+                Class macOSApplication = Class.forName("com.apple.eawt.Application");
+                Method getApplication = Class.forName("com.apple.eawt.Application").getMethod("getApplication");
+                Object application = getApplication.invoke(macOSApplication);
+                Class params[] = new Class[1];
+                params[0] = java.awt.Image.class;
+                Method setDockIconImage = Class.forName("com.apple.eawt.Application").getMethod("setDockIconImage", params);
+                setDockIconImage.invoke(application, SwingFXUtils.fromFXImage(new javafx.scene.image.Image(Main.class.getResourceAsStream("icon.png")), null));
+            } catch (ClassNotFoundException | NoSuchMethodException | InvocationTargetException | IllegalAccessException e) {
+                e.printStackTrace();
+            }
         }
-
         //create scene
         Scene scene = new Scene(root);
 
